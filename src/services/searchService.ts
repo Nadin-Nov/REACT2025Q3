7bb1df3fd637532e089b2388f3bfeaceb62c3834
Nadin Novoselova <nadja.novoselova@gmail.com>
2025-07-17 12:59:56 +0300

    fix: delete comment in searchservice

--- src/services/searchService.ts
+++ src/services/searchService.ts
@@ -1,11 +1,9 @@
 export default class SearchService {
   private static STORAGE_KEY = 'searchTerm';
-  //ищу
   static getSavedSearchTerm(): string {
     const term = localStorage.getItem(SearchService.STORAGE_KEY);
     return term ?? '';
   }
-  //сохраняю
   static saveSearchTerm(term: string): void {
     localStorage.setItem(SearchService.STORAGE_KEY, term);
   }
