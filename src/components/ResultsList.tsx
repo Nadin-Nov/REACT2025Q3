7f1d21be924e2aacd9cd555012ed705298880a94
Nadin Novoselova <nadja.novoselova@gmail.com>
2025-07-25 00:54:47 +0300

    fix: fix import to ResultCard

--- src/components/ResultsList.tsx
+++ src/components/ResultsList.tsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 
 import type { Character } from '../types';
 
-import ResultCard from './ResultCard';
+import {ResultCard} from './ResultCard';
 
 type Props = {
   characters: Character[];
