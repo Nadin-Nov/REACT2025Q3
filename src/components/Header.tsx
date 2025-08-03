f5c346a1edf155c160bbea5829c3832c9ff231aa
Nadin Novoselova <nadja.novoselova@gmail.com>
2025-08-03 17:10:14 +0300

    feat: add theme buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import type { FC } from 'react';
 
 import { NavMenu } from './NavMenu';
+import { ThemeToggle } from './ThemeToggle';
 
 export const Header: FC = () => (
   <header className="header">
     <h1 className="header-title">Get Schwifty</h1>
     <NavMenu />
+    <ThemeToggle />
   </header>
 );
