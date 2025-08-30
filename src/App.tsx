5193a6da147cfd39ee100a85106d67981a6ee033
Nadin Novoselova <nadja.novoselova@gmail.com>
2025-08-31 01:02:33 +0300

    feat: add main and App files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import type { FC } from 'react';
+
+import styles from './App.module.css';
+
+const App: FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>CO₂ Explorer</h1>
+      <p> empty </p>
+    </div>
+  );
+};
+
+export default App;
